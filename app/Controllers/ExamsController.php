b7275a5577f390bce4dc4e1a0a72e7612118cc8e
Iris <irisnogueira@infojr.com.br>
2018-03-26 11:08:10 -0300

    Edições de relacionamento de provas

--- app/Controllers/ExamsController.php
+++ app/Controllers/ExamsController.php
@@ -5,6 +5,7 @@
     use Core\BaseController;
     use Core\Container;
     use Core\Redirect;
+    use Core\Session;
 
     class ExamsController extends BaseController{
 
@@ -19,47 +20,75 @@
         }
 
         public function index(){
+            if(Session::get('success')){
+                $this->view->success = Session::get('success');
+                Session::destroy('success');
+            }
+            if(Session::get('errors')){
+                $this->view->errors = Session::get('errors');
+                Session::destroy('errors');
+            }
             $this->setPageTitle('Provas');
             $this->view->exams = $this->exam->all();
-            $this->renderView('exams/index', 'layout');
+            return $this->renderView('exams/index', 'layout');
         }
 
         public function show($id){
             $this->view->exam = $this->exam->readSingle($id);
             $this->setPageTitle('Prova');
             if ($this->view->exam->id){
-                $this->renderView('exams/show', 'layout');
+                return $this->renderView('exams/show', 'layout');
             } else {
-                $this->renderView('404');
+                return $this->renderView('404');
             }
         }
 
         public function create(){
             $this->getComponents();
             $this->setPageTitle('Nova Prova');
-            $this->renderView('exams/create', 'layout');
+            return $this->renderView('exams/create', 'layout');
         }
 
         public function store($request){
             date_default_timezone_set("America/Bahia");
-            $this->exam->createExam("{$request->post->professor}", "{$request->post->period}", date('Y-m-d'), "{$request->post->components_id}", "{$request->post->unit}");
-            Redirect::route("/exams");
+            if( $this->exam->createExam("{$request->post->professor}", "{$request->post->period}", date('Y-m-d'), "{$request->post->components_id}", "{$request->post->unit}") ){
+                return Redirect::route("/exams", [
+                    'success' => ['Prova enviada para moderação.']
+                ]);
+            } else {
+                return Redirect::route("/exams", [
+                    'errors' => ['Erro ao inserir prova no banco de dados.']
+                ]);
+            }
         }
 
         public function edit($id){
             $this->getComponents();
             $this->view->exam = $this->exam->readSingle($id);
             $this->setPageTitle('Edição de prova');
-            $this->renderView('exams/edit', 'layout');
+            return $this->renderView('exams/edit', 'layout');
         }
 
         public function update($id, $request){
-            $this->exam->update("{$id}", "{$request->post->professor}", "{$request->post->period}", "{$request->post->components_id}","{$request->post->unit}");
-            Redirect::route("/exam/{$id}/show");
+            if ($this->exam->update("{$id}", "{$request->post->professor}", "{$request->post->period}", "{$request->post->components_id}","{$request->post->unit}") ){
+                //Redirect::route("/exam/{$id}/show";
+                return Redirect::route("/exams", [
+                    'success' => ['Prova atualizada com sucesso.']
+                ]);
+            }else {
+                return Redirect::route("/exams", [
+                    'errors' => ['Erro ao atualizar.']
+                ]);
+            }
         }
 
         public function delete($id){
-            $this->exam->deleteExam($id);
-            Redirect::route("/exams");
+            if($this->exam->deleteExam($id)){
+                return Redirect::route("/exams");
+            }else {
+                return Redirect::route("/exams", [
+                    'errors' => ['Erro ao deletar.']
+                ]);
+            }
         }
     }
