c5b37c32d260c7c5f5f538364f1abdc4a9992ac9
danielevalverde <danierzevalverde@gmail.com>
2018-03-27 15:01:50 -0300

    Edição nos controllers para verificar se a session login está setada

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,7 +7,14 @@ use Core\BaseController;
 class HomeController extends BaseController {
 
     public function index() {
+    	if(Session::get('login')){
+            $this->view->login=Session::get('login');
+        }
         $this->setPageTitle("Home");
         $this->renderView('home/index', 'layout');
+    }
+    public function forbiden()
+    {
+        return Redirect::route('/');
     }
 }
