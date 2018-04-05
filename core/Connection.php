2b199845c1d4dcbc3e58cb1700186c3aafbca7cb
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-04-05 00:18:42 -0300

    senha de BD retirada

--- core/Connection.php
+++ core/Connection.php
@@ -7,7 +7,7 @@ use PDOException;
 class Connection {
     public static function connect() {
         try {
-            $pdo = new PDO("mysql:host=localhost;dbname=mydb;charset=utf8;collation=utf8_unicode_ci", "root", 'biscoito');
+            $pdo = new PDO("mysql:host=localhost;dbname=mydb;charset=utf8;collation=utf8_unicode_ci", "root", '');
             $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND , "SET NAMES 'utf8' COLLATE 'utf8_unicode_ci'");
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
