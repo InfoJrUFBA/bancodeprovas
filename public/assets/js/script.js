a7c800057eb80173ba8449b486161662e18f5d80
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-06-16 03:53:28 -0300

    Página de edição de perfil reestilizada
    * Formulário similar aos outros já feitos
    * Seção da mudança de senha alterada, ainda falta atualizar a validação (WIP)
    * Adicionado script com jQuery para ter o slideToggle ao selecionar 'alterar senha'

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,18 +5,29 @@
 //         $(".page-title").addClass("display-4");
 //     }
 // });
+$('#new-pswd').click(function () {
+    if ($('#pswd-group').hasClass("pswd-group")) {
+        $('#pswd-group').slideDown();
+        $('#pswd-group').removeClass("pswd-group");
+        $('#pswd-group').removeAttr("style");
+    } else {
+        $('#pswd-group').slideUp(function() {
+            $('#pswd-group').addClass("pswd-group");
+        });
+    }
+});
 
 $(document).ready(function() {
     $('.display').DataTable();
 });
 
-function activateInsertion() {
+// function activateInsertion() {
 
-    if(userUpdateForm.new_pswd_setting.checked){
-        document.userUpdateForm.new_password.disabled=false;
-        document.userUpdateForm.new_password_confirmation.disabled=false;
-    } else {
-        document.userUpdateForm.new_password.disabled=true;
-        document.userUpdateForm.new_password_confirmation.disabled=true;
-    }
-}
+//     if(userUpdateForm.new_pswd_setting.checked){
+//         document.userUpdateForm.new_password.disabled=false;
+//         document.userUpdateForm.new_password_confirmation.disabled=false;
+//     } else {
+//         document.userUpdateForm.new_password.disabled=true;
+//         document.userUpdateForm.new_password_confirmation.disabled=true;
+//     }
+// }
