3c03bafb998a311021f229e5c237ab0706b74a08
Valdinei Ferreira <valdineiferreira@infojr.com.br>
2018-06-28 20:09:34 -0300

    adicionando novos arquivos do datatable e mudando o CSS das tabelas

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,3 @@
-// $(window).resize(function() {
-//     if ($(window).width() < 740) {
-//         $(".page-title").removeClass("display-4");
-//     } else {
-//         $(".page-title").addClass("display-4");
-//     }
-// });
 $('#new-pswd').click(function () {
     if ($('#pswd-group').hasClass("pswd-group")) {
         $('#pswd-group').slideDown();
@@ -18,16 +11,35 @@ $('#new-pswd').click(function () {
 });
 
 $(document).ready(function() {
-    $('.display').DataTable();
+    $('.datatable').DataTable ({
+        "responsive": true,
+        // "columnDefs": [
+        //         { responsivePriority: 1, targets: 0 },
+        //         { responsivePriority: 2, targets: -1 }
+        // ],
+        "language": {
+            // "url": "/assets/js/datatables-ptBR.lang"
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado... Ainda! :)",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
 });
-
-// function activateInsertion() {
-
-//     if(userUpdateForm.new_pswd_setting.checked){
-//         document.userUpdateForm.new_password.disabled=false;
-//         document.userUpdateForm.new_password_confirmation.disabled=false;
-//     } else {
-//         document.userUpdateForm.new_password.disabled=true;
-//         document.userUpdateForm.new_password_confirmation.disabled=true;
-//     }
-// }
