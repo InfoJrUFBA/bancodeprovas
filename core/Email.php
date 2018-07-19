982b84e366e4778d0e1f3ffca23988a18af74bfc
itamar <itamarjoire@infojr.com.br>
2018-07-19 20:16:29 -0300

    classe Email corrigida

--- core/Email.php
+++ core/Email.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace Core;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-
 include '../vendor/autoload.php';
 
-
-class Email{
-
+class Email {
     public function send ($name, $email, $token, $password = NULL) {
-
         $mail = new PHPMailer(true);
+
         try {
 
             $mail->SMTPDebug = 2;
@@ -20,25 +16,26 @@ class Email{
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
             $mail->Username = 'sistemabdp@gmail.com';
-            $mail->Password = '';
+            $mail->Password = 'melhordiretoria';
             $mail->SMTPSecure = 'ssl';
             $mail->Port = 465;
             $mail->CharSet="utf8";
 
-            $mail->setFrom('sistemabdp@gmail.com', 'Equipe de suporte');
-            $mail->addAddress($_POST["email"], $_POST["name"]);
+            $mail->setFrom('sistemabdp@gmail.com', 'Banco de Provas');
+            $mail->addAddress($email, $name);
 
             $mail->isHTML(true);
-            if(isset($password)){
+
+            if (isset($password)) {
                 $mail->Subject = 'Recuperação de Senha';
                 $mail->Body    = "<p>Olá. Uma nova senha foi solicitada para sua conta no Banco de Prova.<br>
-                                Sua nova senha de acesso é <i>" . $password . "</i>.<br>
+                                Sua nova senha de acesso é <i>{$password}</i>.<br>
                                 Recomendamos que você altere sua senha.";
-            }else{
-                $mail->Subject = 'E-mail de confirmação';
-                $mail->Body    = "<p>Olá, " . $_POST["name"] . ". Seja bem vindo(a) ao nosso Banco de Provas.</p>
+            } else {
+                $mail->Subject = 'E-mail de Confirmação';
+                $mail->Body    = "<p>Olá, {$name}. Seja bem vindo(a) ao nosso Banco de Provas.</p>
                 <p>Seu email ainda não foi confirmado. Por favor, clique no link abaixo para confirmar o seu email.</p>
-                <a href='http://localhost:8000/validation/" . $token . "'>http://localhost:8000/validation/". $token . ".</a>
+                <a href='http://{$_SERVER['HTTP_HOST']}/validation/{$token}'>http://{$_SERVER['HTTP_HOST']}/validation/{$token}</a><br><br>
                 <center><b>Email automático de confirmação. Favor não responder.</b></center>";
             }
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
